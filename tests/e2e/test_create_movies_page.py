3fbea9da30ddc17ba199a2ba1a3cf3f05dd54c02
justicesnowden <123575744+justicesnowden@users.noreply.github.com>
2023-03-22 16:53:00 -0400

    Update test_create_movies_page.py

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,7 @@
 # TODO: Feature 2
+from app import movie_repository
+from flask.testing import FlaskClient
+def test_create_movie(test_app: FlaskClient):
+    movie_repository.create_movie('Scary Movie 2','Keenen Ivory Wayans','3')
+    response = test_app.post('/movies')
+    assert response
