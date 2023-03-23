9ef2b6eace3798897222bb563effa9b25b4cb59d
Kya <kyarash45@gmail.com>
2023-03-22 20:33:31 -0400

    Feature 3 stuff and testing

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,16 @@
-# TODO: Feature 3
+from src.repositories.movie_repository import get_movie_repository
+
+
+random_movie_repo = get_movie_repository()
+
+random_movie_repo.create_movie('Star Wars: The Force Awakens', 'A Director Guy', 2)
+random_movie_repo.create_movie('Spider Man: No Way Home', 'Director Name', 4)
+random_movie_repo.create_movie('The Bee Movie', 'Buzzy Director', 5)
+
+def test_get_movie_by_title():
+
+# testing for a movie that exists
+    assert random_movie_repo.get_movie_by_title('The Bee Movie')
+
+# testing for a movie that doesnt exist
+    assert random_movie_repo.get_movie_by_title('The Iron Giant') == None
