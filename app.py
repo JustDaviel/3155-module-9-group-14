416336c79b602369f3f6b791c2f3e166ba97e25b
Kya <kyarash45@gmail.com>
2023-03-22 20:37:57 -0400

    Feature 3 and tests final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
-
+movie_repository.create_movie("Spider Man: No Way Home", "Director Name, 4 ")
 
 @app.get('/')
 def index():
@@ -33,8 +33,10 @@ def create_movie():
 
 @app.get('/movies/search')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    title = request.args.get('title')
+    movies = movie_repository.get_movie_by_title(title)
+    rating = movie_repository.get_movie_by_title(request.args.get('title')).rating 
+    return render_template('search_movies.html', search_active=True, movies=movies, rating=rating)
 
 
 @app.get('/movies/<int:movie_id>')
